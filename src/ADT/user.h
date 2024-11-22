d4699e22621df18bff178b05f158a580b9f73e73
Alghan pridanusuta <18223058@std.stei.itb.ac.id>
2024-11-22 12:42:47 +0700

    Update ADT
    menambahkan ADT User

---
+++ src/ADT/user.h
@@ -0,0 +1,26 @@
+#ifndef LISTUSER_H
+#define LISTUSER_H
+
+#include <stdio.h>
+#include "boolean.h"
+
+#define MAX_USERS 100
+#define MAX_LEN 50
+
+typedef struct {
+    char name[MAX_LEN];
+    char password[MAX_LEN];
+    int money;
+} User;
+
+typedef struct {
+    User buffer[MAX_USERS];
+    int nEff;
+} ListUser;
+
+void CreateListUser(ListUser *list);
+boolean IsUserExist(ListUser *list, char *username);
+void AddUser(ListUser *list, User newUser);
+User *FindUser(ListUser *list, char *username);
+
+#endif
