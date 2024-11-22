376445e178a2ba4e59fce8c22e9b2c0616a0adb5
khalshaqzzy <19623094@std.stei.itb.ac.id>
2024-11-22 11:44:34 +0700

    Add : ADT barang and listbarang
    added new adt barang and list barang for store and supposedly load & save

---
+++ src/ADT/listbarang.h
@@ -0,0 +1,51 @@
+#ifndef LISTBARANG_H
+#define LISTBARANG_H
+
+#include "boolean.h"
+#include "barang.h"
+#include "mesinkarakter.h"
+
+typedef struct {
+    Barang *buffer;
+    int capacity;
+    int neff;
+} ListBarang;
+
+void InitializeListBarang(ListBarang *list);
+/* I.S. sembarang */
+/* F.S. Sebuah list barang kosong terbentuk dengan kapasitas awal tertentu */
+
+int LengthList(ListBarang list);
+/* Mengirimkan banyaknya elemen list. Mengirimkan 0 jika list kosong */
+
+boolean IsListEmpty(ListBarang list);
+/* Mengirimkan true jika list kosong */
+
+Barang GetBarangAt(ListBarang list, int idx);
+/* Mengembalikan barang pada indeks idx (indeks mulai dari 0) */
+
+void AddBarang(ListBarang *list, Barang item);
+/* Menambahkan barang ke list */
+/* I.S. list mungkin kosong, list tidak penuh */
+/* F.S. item menjadi elemen terakhir yang baru di list */
+
+int FindBarangByName(ListBarang list, char *name);
+/* Mengembalikan indeks barang dengan nama tertentu di list */
+/* Mengembalikan -1 jika tidak ditemukan */
+
+void RemoveBarangAt(ListBarang *list, int idx);
+/* Menghapus barang pada indeks idx dari list */
+/* I.S. idx valid (0 <= idx < LengthList(list)) */
+/* F.S. Barang pada indeks idx dihapus dari list */
+
+void DisplayListBarang(ListBarang list);
+/* Menampilkan daftar barang yang ada di list */
+/* I.S. list mungkin kosong */
+/* F.S. Jika list tidak kosong, menampilkan nama barang yang ada di list */
+
+void FreeListBarang(ListBarang *list);
+/* Membebaskan memori yang dialokasikan untuk list */
+/* I.S. list terdefinisi */
+/* F.S. Memori untuk buffer list dibebaskan */
+
+#endif
