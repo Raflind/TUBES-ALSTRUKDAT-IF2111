e672252ed826f5f820a810b06edb78917260d2c8
ShirakamiFubuki <Fubuki@Fox.Hambuger>
2024-11-23 11:05:05 +0700

    fix : help.c, add : help_drive.c

--- src/help.c
+++ src/help.c
@@ -2,39 +2,27 @@
 
 void help_welcome(){
     printf("=====[ Menu Help PURRMART]=====\n");
-    const char *list[] = {
-        "START -> Untuk masuk sesi baru",
-        "LOAD -> Untuk memulai sesi berdasarkan file konfigurasi",
-        "QUIT -> Untuk keluar dari program"};
-    for (int i = 0; i < 3; i++){
-        printf("%d. %s\n", i + 1, list[i]);
-    }
+    printf("START -> Untuk masuk sesi baru\n");
+    printf("LOAD -> Untuk memulai sesi berdasarkan file konfigurasi\n");
+    printf("QUIT -> Untuk keluar dari program\n");
 }
 
 void help_login(){
     printf("=====[ Menu Help PURRMART]=====\n");
-    const char *list[] = {
-        "REGISTER -> Untuk melakukan pendaftaran akun baru",
-        "LOGIN -> Untuk masuk ke dalam akun dan memulai sesi",
-        "QUIT -> Untuk keluar dari program"};
-    for (int i = 0; i < 3; i++){
-        printf("%d. %s\n", i + 1, list[i]);
-    }
+    printf("REGISTER -> Untuk melakukan pendaftaran akun baru\n");
+    printf("LOGIN -> Untuk masuk ke dalam akun dan memulai sesi\n");
+    printf("QUIT -> Untuk keluar dari program\n");
 }
 
 void help_main(){
     printf("=====[ Menu Help PURRMART]=====\n");
-    const char *list[] ={
-        "WORK -> Untuk bekerja",
-        "WORK CHALLENGE -> Untuk mengerjakan challenge",
-        "STORE LIST -> Untuk melihat barang-barang di toko",
-        "STORE REQUEST -> Untuk meminta penambahan barang",
-        "STORE SUPPLY -> Untuk menambahkan barang dari permintaan",
-        "STORE REMOVE -> Untuk menghapus barang",
-        "LOGOUT -> Untuk keluar dari sesi",
-        "SAVE -> Untuk menyimpan state ke dalam file",
-        "QUIT -> Untuk keluar dari program"};
-    for (int i = 0; i < 9; i++){
-        printf("%d. %s\n", i + 1, list[i]);
-    }
+    printf("WORK -> Untuk bekerja\n");
+    printf("WORK CHALLENGE -> Untuk mengerjakan challenge\n");
+    printf("STORE LIST -> Untuk melihat barang-barang di toko\n");
+    printf("STORE REQUEST -> Untuk meminta penambahan barang\n");
+    printf("STORE SUPPLY -> Untuk menambahkan barang dari permintaan\n");
+    printf("STORE REMOVE -> Untuk menghapus barang\n");
+    printf("LOGOUT -> Untuk keluar dari sesi\n");
+    printf("SAVE -> Untuk menyimpan state ke dalam file\n");
+    printf("QUIT -> Untuk keluar dari program\n");
 }
