a5dfc97fedb0984ba9d547e85fc978a5dbadaa5f
Michael-Ballard-Isaiah-Silaen <silaenmichael@gmail.com>
2024-11-24 00:30:02 +0700

    Created drive for Load and Start

---
+++ src/start_drive.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "start.h"
+#include "listbarang.h"
+#include "user.h"
+
+int main(){
+  ListUser listuserz;
+  ListBarang listbarangz;
+  CreateListUser(&listuserz);
+  InitializeListBarang(&listbarangz);
+  Startr(&listuserz, &listbarangz);
+  printf("\n");
+  int tempi;
+  for(int i=0; i<listuserz.nEff; i++){
+    printf("%d ", listuserz.buffer[i].money);
+    tempi = strlent(listuserz.buffer[i].name);
+    for(int j=0; j<tempi; j++){
+      printf("%c", listuserz.buffer[i].name[j]);
+    }
+    printf(" ");
+    tempi = strlent(listuserz.buffer[i].password);
+    for(int j=0; j<tempi; j++){
+      printf("%c", listuserz.buffer[i].password[j]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+  for(int i=0; i<listbarangz.neff; i++){
+    printf("%d ", listbarangz.buffer[i].price);
+    tempi = strlent(listbarangz.buffer[i].name);
+    for(int j=0; j<tempi; j++){
+      printf("%c", listbarangz.buffer[i].name[j]);
+    }
+    printf("\n");
+  }
+}
