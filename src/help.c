69356afce711c141bf5242ee113b629b63ff087e
ShirakamiFubuki <Fubuki@Fox.Hambuger>
2024-12-20 03:14:09 +0700

    Update help.c and add file_konfig2.txt

--- src/help.c
+++ src/help.c
@@ -1,21 +1,21 @@
 #include "help.h"
 
 void help_welcome(){
-    printf("=====[ Menu Help PURRMART]=====\n");
+    printf("=====[ Menu Help PURRMART ]=====\n");
     printf("START -> Untuk masuk sesi baru\n");
     printf("LOAD -> Untuk memulai sesi berdasarkan file konfigurasi\n");
     printf("QUIT -> Untuk keluar dari program\n");
 }
 
 void help_login(){
-    printf("=====[ Menu Help PURRMART]=====\n");
+    printf("=====[ Menu Help PURRMART ]=====\n");
     printf("REGISTER -> Untuk melakukan pendaftaran akun baru\n");
     printf("LOGIN -> Untuk masuk ke dalam akun dan memulai sesi\n");
     printf("QUIT -> Untuk keluar dari program\n");
 }
 
 void help_main(){
-    printf("=====[ Menu Help PURRMART]=====\n");
+    printf("=====[ Menu Help PURRMART ]=====\n");
     printf("WORK -> Untuk bekerja\n");
     printf("WORK CHALLENGE -> Untuk mengerjakan challenge\n");
     printf("STORE LIST -> Untuk melihat barang-barang di toko\n");
@@ -25,4 +25,16 @@ void help_main(){
     printf("LOGOUT -> Untuk keluar dari sesi\n");
     printf("SAVE -> Untuk menyimpan state ke dalam file\n");
     printf("QUIT -> Untuk keluar dari program\n");
+    printf("PROFILE -> Untuk melihat data diri user\n");
+    prtinf("HISTORY <n> -> Menampilkan riwayat <n> pembelian terbaru\n");
+    printf("CART ADD <nama> <n> -> Menambahkan barang sebanyak <n>\n");
+    printf("CART REMOVE <name> <n> -> Menghapus barang sebanyak <n>\n");
+    printf("CART SHOW -> Menampilkan semua barang dalam CART\n");
+    printf("CART PAY -> Membeli semua barang asal uang user cukup\n");
+    printf("WISHLIST ADD -> Menambah barang ke wishlist\n");
+    printf("WWISHLIST SWAP <i> <j> -> Menukar posisi barang dalam wishlist\n");
+    printf("WISHLIST REMOVE <i> -> Menghapus barang pada posisi <i>\n");
+    printf("WISHLIST REMOVE -> Menghapus barang sesuai nama\n");
+    printf("WISHLIST CLEAR -> Menghapus semua barang dalam wishlist\n");
+    printf("WISHLIST SHOW -> Menampilkan wishlist user\n");
 }
