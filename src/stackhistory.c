6dbc4040f01aa6d02e3f456cb0fc02e3a713758c
khalshaqzzy <19623094@std.stei.itb.ac.id>
2024-12-21 19:30:30 +0700

    update: updated stackhistory with linkedlist
    created new struct for stackhistory using linkedlist to avoid overflow

--- src/stackhistory.c
+++ src/stackhistory.c
@@ -1,66 +1,68 @@
 #include "stackhistory.h"
-#include <stdio.h>
 
-void CreateEmptyStack(Stack *S){
-    Top(*S) = Nils;
+void CreateEmptyStack(Stack *S) {
+    S->TOP = NULL;
 }
 
-boolean IsStackEmpty(Stack S){
-    return (Top(S) == Nils);
+boolean IsStackEmpty(Stack S) {
+    return (S.TOP == NULL);
 }
 
-boolean IsStackFull(Stack S){
-    return (Top(S) == MaxEl - 1);
+boolean IsStackFull(Stack S) {
+    return false;
 }
 
-void Push(Stack * S, infotype X){
-    Top(*S)++;
-    S->T[Top(*S)] = X;
+void Push(Stack *S, infotype X) {
+    address newNode = (address) malloc(sizeof(stacknode));
+    newNode->info = X;
+    newNode->next = S->TOP;
+    S->TOP = newNode;
 }
 
-void Pop(Stack * S, infotype* X){
-    *X = S->T[Top(*S)];
-    Top(*S)--;
+void Pop(Stack *S, infotype *X) {
+    if (!IsStackEmpty(*S)) {
+        address temp = S->TOP;
+        *X = temp->info;
+        S->TOP = temp->next;
+        free(temp);
+    }
 }
 
-void PrintStackHistory(Stack S, int N){
+void PrintStackHistory(Stack S, int N) {
     if (IsStackEmpty(S)) {
         printf("Kamu belum membeli barang apapun!\n");
         return;
     }
-
-    int total_pembelian = Top(S) + 1;
-    int jumlah_dicetak;
-
-    if (N > total_pembelian) {
-        jumlah_dicetak = total_pembelian;
-    } 
-
-    else {
-        jumlah_dicetak = N;
-    }
-
-    printf("Riwayat Pembelian : \n");
-    for(int i = Top(S); i >= Top(S) - jumlah_dicetak + 1; i--){
-        if(i < 0) break;
-        Pembelian p = S.T[i];
+    printf("Riwayat Pembelian:\n");
+    address current = S.TOP;
+    int count = 0;
+    while (current != NULL && count < N) {
+        Pembelian p = current->info;
         printf("Pembelian %d - Total %d\n", p.id, p.total);
-        printf("Kuantitas\tNama\tTotal\n");
-        for(int j = 0; j < p.jumlah_barang; j++) {
-            Cart cart = p.cart[j];
-            printf("%d\t\t%s\t%d\n", cart.Kuantitas, cart.Barang.name, cart.total);
+        printf("Kuantitas\tNama\t\tTotal\n");
+        for (int j = 0; j < p.jumlah_barang; j++) {
+            Cart cartItem = p.cart[j];
+            printf("%d\t\t%s\t\t%d\n", cartItem.Kuantitas, cartItem.Barang.name, cartItem.total);
         }
         printf("\n");
+        current = current->next;
+        count++;
     }
 }
 
-void AddtoPembelian(Map *M, Stack *stackhistory){
+void AddtoPembelian(Map *M, Stack *stackhistory) {
     Pembelian purchase;
-    for(int i = 0; i < M->Count;i++){
+    purchase.jumlah_barang = 0;
+    purchase.total = 0;
+    for(int i = 0; i < M->Count; i++){
         purchase.cart[i] = M->Elements[i];
     }
-    purchase.jumlah_barang += M->Count;
-    purchase.total += TotalHarga(*M);
-    purchase.id = stackhistory->TOP + 1;
+    purchase.jumlah_barang = M->Count;
+    purchase.total = TotalHarga(*M);
+    if(IsStackEmpty(*stackhistory)) {
+        purchase.id = 1;
+    } else {
+        purchase.id = stackhistory->TOP->info.id + 1;
+    }
     Push(stackhistory, purchase);
 }
--- src/stackhistory.h
+++ src/stackhistory.h
@@ -4,61 +4,36 @@
 #include "boolean.h"
 #include "barang.h"
 #include "cart.h"
-
-#define Nils -1
-#define MaxEl 100
-#define MaxItems 10
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct {
     int id;
     int total;
     int jumlah_barang;
-    Cart cart[MaxItems];
+    Cart cart[10];
 } Pembelian;
 
-
 typedef Pembelian infotype;
-typedef int address;
 
-typedef struct { 
-    infotype T[MaxEl];
+typedef struct stacknode *address;
+
+typedef struct stacknode {
+    infotype info;
+    address next;
+} stacknode;
+
+typedef struct {
     address TOP;
 } Stack;
 
-#define Top(S) (S).TOP
-#define InfoTop(S) (S).T[(S).TOP]
 
-/* ************ Prototype ************ */
-/* *** Konstruktor/Kreator *** */
 void CreateEmptyStack(Stack *S);
-/* I.S. sembarang; */
-/* F.S. Membuat sebuah stack S yang kosong berkapasitas MaxEl */
-/* jadi indeksnya antara 0.. MaxEl */
-/* Ciri stack kosong : TOP bernilai Nil */
-
-/* ************ Predikat Untuk test keadaan KOLEKSI ************ */
 boolean IsStackEmpty(Stack S);
-/* Mengirim true jika Stack kosong: lihat definisi di atas */
 boolean IsStackFull(Stack S);
-/* Mengirim true jika tabel penampung nilai elemen stack penuh */
-
-/* ************ Menambahkan sebuah elemen ke Stack ************ */
-void Push(Stack * S, infotype X);
-/* Menambahkan X sebagai elemen Stack S. */
-/* I.S. S mungkin kosong, tabel penampung elemen stack TIDAK penuh */
-/* F.S. X menjadi TOP yang baru,TOP bertambah 1 */
-
-/* ************ Menghapus sebuah elemen Stack ************ */
-void Pop(Stack * S, infotype* X);
-/* Menghapus X dari Stack S. */
-/* I.S. S  tidak mungkin kosong */
-/* F.S. X adalah nilai elemen TOP yang lama, TOP berkurang 1 */
-
-
+void Push(Stack *S, infotype X);
+void Pop(Stack *S, infotype *X);
 void PrintStackHistory(Stack S, int N);
-// Menampilkan riwayat pembelian sebanyak N, dimulai dari pembelian terbaru (top element)
-// Stack mungkin kosong, N mungkin lebih banyak dari neff stack
-
 void AddtoPembelian(Map *M, Stack *stackhistory);
 
 #endif
