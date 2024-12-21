a8b2412e97a9e50070ac270753e7217a54069637
Pandalism <18223038@std.stei.itb.ac.id>
2024-12-21 22:38:45 +0700

    fix: user, cart

--- src/cart.h
+++ src/cart.h
@@ -11,8 +11,6 @@ void CartAdd(Map *M, ListBarang list, char *name, int quantity);
 
 void CartRemove(Map *M, ListBarang list, char *name, int quantity);
 
-void DisplayCart(Map M);
-
 int TotalHarga(Map M);
 
 #endif
