5fbc46c7dd19976029f0afd5da3a1b2bec5ec068
Pandalism <18223038@std.stei.itb.ac.id>
2024-12-22 19:47:13 +0700

    fix: shell script, batch file

--- src/run.sh
+++
@@ -1,3 +0,0 @@
-gcc main.c command/cart.c command/help.c ADT/linkedlist.c ADT/listbarang.c command/load.c command/login.c command/logout.c ADT/map.c ADT/mesinkarakter.c ADT/mesinkata.c command/newload.c command/newstart.c command/profile.c ADT/queue.c command/quit.c command/register.c command/save.c ADT/stackhistory.c command/store.c command/tebakangka.c ADT/user.c command/wishlist.c command/wordl3.c command/works.c command/optimasirute.c -o main
-
-./main
